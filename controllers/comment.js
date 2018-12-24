0c6a8e83524382ed7ca918f1756aeef7e6ef9246
Kamil Kozik <36657834+kozikkam@users.noreply.github.com>
2018-12-24 20:31:38 +0100

    Update comment.js

--- controllers/comment.js
+++ controllers/comment.js
@@ -1,38 +1,42 @@
 const Comment = require('../models/index').comment;
 const Movie = require('../models/index').movie;
-const checkForArguments = require('./helpers').checkForArguments;
-const movieFindOne = require('./helpers').movieFindOne;
+const { checkForArguments } = require('./helpers');
+const { movieFindOne } = require('./helpers');
 
 module.exports = {
     async create(req, res) {
-        let id = req.body.id;
-        let body = req.body.body;
+        let { id } = req.body;
+        let { body } = req.body;
+        
         checkForArguments({
-            id: id,
-            body: body
+            id,
+            body
         }, res);
-        id = parseInt(req.body.id);
-        console.log(id);
-        let movie = await movieFindOne({id: id});
+        
+        id = parseInt(id);
+        let movie = await movieFindOne({id});
+        
         if(!movie){
             res.status(400);
             throw new Error("No movie with given id");
         }
-        let comment = await Comment.create({
-            body: body
-        });
+        
+        let comment = await Comment.create({ body });
+        
         await movie.addComment(comment);
+        
         res.status(200);
         return comment;
     },
     async list(req, res) {
         let conditions = {};
+        
         if(req.query.movieId) {
-            conditions.movieId = req.query.movieId;
+            Object.assign(conditions, { movieId: req.query.movieId });
         }
-        let comments = await Comment.findAll({
-            where: conditions
-        });
+        
+        let comments = await Comment.findAll({ where: conditions });
+        
         return comments;
     }
 };
