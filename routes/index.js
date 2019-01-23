737aaebee040529870a07983dbd28f92f2787603
Kamil Kozik <36657834+kozikkam@users.noreply.github.com>
2019-01-23 16:12:40 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,25 @@
-const moviesController = require('../controllers/index').movies;
-const commentsController = require('../controllers/index').comments;
+const { movies, comments } = require('../controllers/index');
 
 module.exports = (app) => {
-    app.get('/', (req, res) => {res.status(200).send('index')});
+    app.get('/', (req, res) => { res.status(200).send('index') });
     app.post('/movies', (req, res) => {
-        moviesController.create(req, res)
+        movies.create(req, res)
             .then(data => res.send(data))
-            .catch(err => {console.log(err); res.send(err)});
+            .catch((err) => { console.log(err); res.send(err); });
     });
     app.get('/movies', (req, res) => {
-        moviesController.list(req, res)
+        movies.list(req, res)
             .then(data => res.status(200).send(data))
-            .catch(err => {console.log(err); res.send(err)});
+            .catch((err) => { console.log(err); res.send(err); });
     });
     app.post('/comments', (req, res) => {
-        commentsController.create(req, res)
+        comments.create(req, res)
             .then(data => res.send(data))
-            .catch(err => {console.log(err); res.send(err)});
+            .catch((err) => { console.log(err); res.send(err); });
     });
     app.get('/comments', (req, res) => {
-        commentsController.list(req, res)
+        comments.list(req, res)
             .then(data => res.status(200).send(data))
-            .catch(err => {console.log(err); res.send(err)});
+            .catch((err) => { console.log(err); res.send(err); });
     });
 }
